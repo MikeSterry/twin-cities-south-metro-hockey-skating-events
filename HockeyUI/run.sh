ff8b13d32afbb5fa40f8f2288ce7bf8d32697ec2
Mike Sterry <msterry@users.noreply.github.com>
2025-12-23 15:59:31 -0600

    feat(favicons): Added favicons - Added favicons - Fixed UI run.sh to use correct port

--- HockeyUI/run.sh
+++ HockeyUI/run.sh
@@ -2,6 +2,6 @@ docker stop hockey-ui
 docker rm hockey-ui
 
 docker build -t hockey-ui .
-docker run -it --rm --name hockey-ui -p 3600:80 \
+docker run -it --rm --name hockey-ui -p 3600:8080 \
   -e REACT_APP_API_URL=http://localhost:5600/api/public_skate_events \
   hockey-ui
