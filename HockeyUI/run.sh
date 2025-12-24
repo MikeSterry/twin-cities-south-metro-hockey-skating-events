c45ff675bed1330af4b9d45d5a861c5d369ef192
msterry <msterry@spscommerce.com>
2025-12-24 11:12:41 -0600

    feat(style): Update to a darker style - Updated to a darker style - Updated image perms - Added an image to use for the top of the main page at some point - Attempted to add an image above the event list, but ended up making it a todo for a later date

--- HockeyUI/run.sh
+++ HockeyUI/run.sh
@@ -1,7 +1,10 @@
+API_URL=$1
+echo "Using API URL: ${API_URL:=localhost}"
+
 docker stop hockey-ui
 docker rm hockey-ui
 
-docker build -t hockey-ui .
+docker build -t hockey-ui . --no-cache
 docker run -it --rm --name hockey-ui -p 3600:8080 \
-  -e REACT_APP_API_URL=http://localhost:5600/api/public_skate_events \
+  -e REACT_APP_API_URL=http://${API_URL:=localhost}:5600/api/public_skate_events \
   hockey-ui
