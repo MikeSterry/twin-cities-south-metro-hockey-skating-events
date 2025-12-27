2ff346a88f8a262299b61ce4c9ee7549e564245b
msterry <msterry@spscommerce.com>
2025-12-27 16:25:40 -0600

    feat(Endpoint): Change API endpoint - Updated API endpoint to a more simple "get_events"

--- HockeyUI/run.sh
+++ HockeyUI/run.sh
@@ -6,5 +6,5 @@ docker rm hockey-ui
 
 docker build -t hockey-ui . --no-cache
 docker run -it --rm --name hockey-ui -p 3600:8080 \
-  -e REACT_APP_API_URL=http://${API_URL:=localhost}:5600/api/public_skate_events \
+  -e REACT_APP_API_URL=http://${API_URL:=localhost}:5600/api/get_events \
   hockey-ui
